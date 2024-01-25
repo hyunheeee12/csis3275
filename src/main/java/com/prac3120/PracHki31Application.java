d5448a6d9d80336b6ba7d48ab08f97b1e0182149
hyunhee <kimh134@student.douglascollege.ca>
2024-01-24 17:25:59 -0800

    final

---
+++ src/main/java/com/prac3120/PracHki31Application.java
@@ -0,0 +1,13 @@
+package com.prac3120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracHki31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracHki31Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/prac3120/PracHki31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac3120;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracHki31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
